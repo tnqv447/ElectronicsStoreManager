4cec83ba6aa4e1f7126a90e5a234a9c0798fe3bf
hieukut456 <trunghieu9946@gmail.com>
2020-12-14 08:02:52 +0700

    not thing

--- MvcClient/Startup.cs
+++ MvcClient/Startup.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using AppCore.Interfaces;
 using AppCore.Services;
 using Infrastructure;
@@ -9,9 +7,7 @@ using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
